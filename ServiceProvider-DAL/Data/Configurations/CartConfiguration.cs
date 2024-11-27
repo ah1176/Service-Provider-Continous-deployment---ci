7ab3d2ef876277871ececc2a94d8be5a847d4c4f
Amir Mawla <132608399+AmirMawla@users.noreply.github.com>
2024-11-27 18:46:48 +0200

    Add files via upload

---
+++ ServiceProvider-DAL/Data/Configurations/CartConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceProvider_DAL.Data.Configurations
+{
+    internal class CartConfiguration
+    {
+    }
+}
